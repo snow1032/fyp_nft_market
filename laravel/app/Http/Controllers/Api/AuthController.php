9548caec05a02492a6fad858927a2217fd1f110f
Chung2212 <123064890+Chung22219056@users.noreply.github.com>
2023-09-16 00:36:57 +0800

    create laravel

---
+++ laravel/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Passport\RefreshToken;
+use Laravel\Passport\Token;
+
+
+class AuthController extends Controller
+{
+/**
+ * @OA\Post(
+ *     path="/login",
+ *     @OA\Parameter(
+ *         name="email",
+ *         required=true,
+ *         in="query",
+ *         @OA\Schema(
+ *             type="string"
+ *         )
+ *     ),
+ *     @OA\Parameter(
+ *         name="password",
+ *         required=true,
+ *         in="query",
+ *         @OA\Schema(
+ *             type="string"
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response="200",
+ *         description="login function",
+ *         @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(
+ *                 @OA\Property(
+ *                     property="user",
+ *                     type="string"
+ *                 ),
+ *                 @OA\Property(
+ *                     property="token",
+ *                     type="string"
+ *                 )
+ *             )
+ *         )
+ *     )
+ * )
+ */
+    public function login(Request $request){
+        $credentials = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required',
+        ]);
+
+        if(!Auth::attempt($credentials)){
+            return response([
+                'message' => 'Provided email or password is incorrect'
+            ], 422);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+        return response(compact('user', 'token'));
+    }
+
+    /**
+     * @OA\Post(
+     *     path="/register",
+     *     summary="Register a new user",
+     *     @OA\RequestBody(
+     *         description="User registration data",
+     *         required=true,
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="name",
+     *                 type="string",
+     *                 maxLength=255
+     *             ),
+     *             @OA\Property(
+     *                 property="email",
+     *                 type="string",
+     *                 format="email",
+     *                 maxLength=255
+     *             ),
+     *             @OA\Property(
+     *                 property="password",
+     *                 type="string",
+     *                 minLength=8
+     *             ),
+     *             @OA\Property(
+     *                 property="confirm_password",
+     *                 type="string",
+     *                 minLength=8
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Registration successful",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="status",
+     *                 type="boolean",
+     *                 example=true
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="default",
+     *         description="Error response",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="status",
+     *                 type="boolean",
+     *                 example=false
+     *             )
+     *         )
+     *     )
+     * )
+     */
+    public function register(Request $request){
+        
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|same:confirm_password',
+        ]);
+
+        $user = new User();
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = Hash::make($request->input('password'));
+        $user->save();
+
+        Auth::login($user);
+
+        return response()->json(['status' => true], 200);
+    }
+
+/**
+ * @OA\Post(
+ *     path="/logout",
+ *     summary="Logout the user",
+ *     @OA\Response(
+ *         response="200",
+ *         description="Logout operation response",
+ *         @OA\JsonContent(
+ *             @OA\Property(
+ *                 property="status",
+ *                 type="boolean",
+ *                 example=true
+ *             )
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response="default",
+ *         description="Error response",
+ *         @OA\JsonContent(
+ *             @OA\Property(
+ *                 property="status",
+ *                 type="boolean",
+ *                 example=false
+ *             )
+ *         )
+ *     )
+ * )
+ */
+
+    public function logout(Rquest $request){
+        try{
+            $user = $request->user();
+            $user->currentAccessToken()->delete();
+            return response()->json(['status' => true], 200);
+        }catch (Exception $ex){
+            return response()->json(['status' => false], 200);
+        }
+    }
+
+}
