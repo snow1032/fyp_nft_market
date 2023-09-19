e142b509cbfdcb1b68f67dea20a305739163d314
PoonKaYuen <89974187+Snowxdd@users.noreply.github.com>
2023-09-20 03:09:37 +0800

    react connect MetaMask

--- react/src/components/Header/Header.jsx
+++ react/src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "./header.css";
 import { Container } from "reactstrap";
-
 import { NavLink, Link } from "react-router-dom";
+import {ethers} from 'ethers';
+
 
 const NAV__LINKS = [
   {
@@ -21,7 +22,7 @@ const NAV__LINKS = [
     display: "Contact",
     url: "/contact",
   },
-   {
+  {
     display: "Login",
     url: "/login",
   },
@@ -31,6 +32,9 @@ const Header = () => {
   const headerRef = useRef(null);
 
   const menuRef = useRef(null);
+
+  const [walletAddress, setWalletAddress] = useState(null)
+  const [userBalance, setUserBalance] = useState(null)
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -45,11 +49,54 @@ const Header = () => {
     });
 
     return () => {
-      window.removeEventListener("scroll");
+      window.removeEventListener("scroll",null);
     };
   }, []);
 
   const toggleMenu = () => menuRef.current.classList.toggle("active__menu");
+
+
+  async function connectWallentAccount(e) {
+    console.log("Requesting account......")
+
+    if (window.ethereum) {
+      console.log("detected")
+
+
+      try {
+        const account = await window.ethereum.request({
+          method: "eth_requestAccounts",
+
+        });
+
+        getUserBalance(account[0]);
+        setWalletAddress(account[0]);
+
+
+
+        console.log(account);
+      } catch (error) {
+        console.log("Error connecting ..")
+      }
+
+    } else {
+      console.log("Meta Mask not dectected")
+    }
+  }
+
+  const getUserBalance = (address) => {
+    window.ethereum.request({
+      method: "eth_getBalance",
+      params:[address,'latest']
+    }).then(balance=>{
+      // setUserBalance(ethers.utils.formatEther(balance))
+      setUserBalance(ethers.formatEther(balance))
+     
+    })
+ 
+  }
+ 
+
 
   return (
     <header className="header" ref={headerRef}>
@@ -82,20 +129,23 @@ const Header = () => {
           </div>
 
           <div className="nav__right d-flex align-items-center gap-5 ">
-            <button className="btn d-flex gap-2 align-items-center">
+            <button className="btn d-flex gap-2 align-items-center" onClick={connectWallentAccount}>
               <span>
                 <i class="ri-wallet-line"></i>
               </span>
-              <Link to="/wallet">Connect Wallet</Link>
+              <Link to="/wallet">{walletAddress ? walletAddress : "Connect Wallets"}</Link>
+             
             </button>
-
+          
             <span className="mobile__menu">
               <i class="ri-menu-line" onClick={toggleMenu}></i>
             </span>
           </div>
         </div>
       </Container>
+      <h1>ETH: {userBalance}</h1>
     </header>
+    
   );
 };
 
