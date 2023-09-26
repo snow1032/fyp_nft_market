a539eba78c66a41266f254a4bcfb9e61b26adbd5
Chung2212 <123064890+Chung22219056@users.noreply.github.com>
2023-09-26 14:19:33 +0800

    fixed chatbot

--- react/src/components/ChatBot/chatBot.jsx
+++ react/src/components/ChatBot/chatBot.jsx
@@ -9,34 +9,34 @@ import { Link } from "react-router-dom";
 
 export default function ChatBot(props) {
 
+  useEffect(()=>{
+    console.log(document.querySelector("chatbox"))
+  },[])
 
   return (
     <>
-      {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,1,0" />
-      <button ClassName="chatbot-toggler">
-        <span ClassName="material-symbols-rounded">mode_comment</span>
-        <span ClassName="material-symbols-outlined">close</span>
-      </button><div ClassName="chatbot">
+      <button className="chatbot-toggler">
+        <span className="material-symbols-rounded">mode_comment</span>
+        <span className="material-symbols-outlined">close</span>
+      </button><div className="chatbot">
         <header>
           <h2>Chatbot</h2>
-          <span ClassName="close-btn material-symbols-outlined">close</span>
+          <span className="close-btn material-symbols-outlined">close</span>
         </header>
-        <ul ClassName="chatbox">
-          <li ClassName="chat incoming">
-            <span ClassName="material-symbols-outlined">smart_toy</span>
+        <ul className="chatbox">
+          <li className="chat incoming">
+            <span className="material-symbols-outlined">smart_toy</span>
             <p>Hi there 👋How can I help you today?</p>
           </li>
         </ul>
-        <div ClassName="chat-input">
+        <div className="chat-input">
           <textarea placeholder="Enter a message..." spellcheck="false" required></textarea>
           <span id="send-btn" ClassName="material-symbols-rounded">send</span>
         </div>
-      </div> */}
-      <div>123</div>
-
+      </div> 
     </>
-
   );
 
 };
